7ad534cc32084302fb52868ac7be02e0d1a2cf3a
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-29 00:33:36 +0600

    new code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./components/About/About";
 import Friends from "./components/Friends/Friends";
@@ -6,9 +7,12 @@ import Home from "./components/Home/Home";
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
+      <h1>Welcome to my Routing website</h1>\
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/friends" element={<Friends></Friends>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
